9e4422b6dd5a92d82697ea7dfc2c7869165db36d
Николай Баишев <baishev.nk@stud.s-vfu.ru>
2022-01-11 20:36:50 +0900

    Вторая попытка

---
+++ Lab17/Account.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab17
+{
+    class Account<T>
+    {
+        private T num;
+        private double balance;
+        private string name;
+
+        public T Num
+        {
+            get
+            {
+                return num;
+            }
+            set
+            {
+                num = value;
+            }
+        }
+
+        public double Balance
+        {
+            get
+            {
+                return balance;
+            }
+            set
+            {
+                balance = value;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine("Номер счета: {0}",num);
+            Console.WriteLine("Баланс: {0:C2}", balance);
+            Console.WriteLine("ФИО: {0}", name);
+        }
+    }
+}
---
+++ Lab17/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account<byte> account1 = new Account<byte>();
+            account1.Num = 1;
+            account1.Balance = 1000000;
+            account1.Name = "Иванов Иван Иванович";
+            account1.ShowInfo();
+            Account<string> account2 = new Account<string>();
+            account2.Num = "007";
+            account2.Balance = 777;
+            account2.Name = "James Bond";
+            Console.WriteLine(account2.Num);
+            Console.WriteLine(account2.Balance);
+            Console.WriteLine(account2.Name);
+            Account<int> account3 = new Account<int>();
+            account3.Num = 100000;
+            account3.Balance = 100;
+            account3.Name = "Скрудж Макдак";
+            account3.ShowInfo();
+            Console.ReadKey();
+        }
+    }
+}
